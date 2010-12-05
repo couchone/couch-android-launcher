be86cb407bdba6077cd8a0fbbd80b30c6fedec5c
Dale Harvey <dale@arandomurl.com>
2010-12-05 03:32:21 +0000

    changed to use remote service for async starting

--- src/org/couchdb/android/CouchDB.java
+++ src/org/couchdb/android/CouchDB.java
@@ -1,52 +1,82 @@
 package org.couchdb.android;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningServiceInfo;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.webkit.WebView;
 
 public class CouchDB extends Activity {
-	public final static String TAG = "CouchDB";
-    Button startButton;
-    Button stopButton;
-    
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-		if(startButton == null) return;
-		ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-
-		List<ActivityManager.RunningServiceInfo> serviceList = activityManager.getRunningServices(Integer.MAX_VALUE);
-
-		if (!(serviceList.size() > 0)) {
-			return;
-		}
-
-		for (int i = 0; i < serviceList.size(); i++) {
-			RunningServiceInfo serviceInfo = serviceList.get(i);
-			ComponentName serviceName = serviceInfo.service;
-
-			if (serviceName.getClassName().equals("org.couchdb.android.CouchService")) {
-				startButton.setEnabled(false);
-				stopButton.setEnabled(true);
-				return;
-			}
-		}
-		startButton.setEnabled(true);
-		stopButton.setEnabled(false);
-	}
 	
+	public final static String TAG = "CouchDB";
+	public final static String FUTON = "http://127.0.0.1:5984/_utils/";
+    
+    Messenger mService = null;
+    
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case CouchService.MSG_COUCH_STARTED:
+                	Log.v(TAG, "Received COUCH_STARTED Message");
+                	// TODO: WebView screws up layout, launching browser
+                	// makes Resume etc annoying
+                	WebView webview = new WebView(CouchDB.this);
+                	setContentView(webview);
+                	webview.loadUrl(FUTON);
+                	//launchFuton();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+        	mService = new Messenger(service);
+            // We want to monitor the service for as long as we are
+            // connected to it.
+            try {
+                Message msg = Message.obtain(null, CouchService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            } catch (RemoteException e) { }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+        }
+    };
+
+    private void launchFuton() { 
+    	Uri uri = Uri.parse(FUTON);
+    	Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    	startActivity(intent);    
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	//launchFuton();
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    MenuInflater inflater = getMenuInflater();
@@ -66,32 +96,16 @@ public class CouchDB extends Activity {
 	    }
 	}
 	
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-        boolean installed = CouchInstaller.checkInstalled();
-        if(!installed)
-        	startActivity(new Intent(this, CouchInstallActivity.class));
-        startButton = (Button) findViewById(R.id.StartButton);
-        stopButton = (Button) findViewById(R.id.StopButton);
-        startButton.setOnClickListener(new OnClickListener(){
 
-			public void onClick(View v) {
-				startService(new Intent("org.couchdb.android.COUCHDB_SERVICE"));
-				startButton.setEnabled(false);
-				stopButton.setEnabled(true);
-			}
-        	
-        });
-        
-        stopButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View v) {
-				stopService(new Intent("org.couchdb.android.COUCHDB_SERVICE"));
-				startButton.setEnabled(true);
-				stopButton.setEnabled(false);
-			}   	
-        });
+		boolean installed = CouchInstaller.checkInstalled();
+        if (!installed) { 
+        	startActivity(new Intent(this, CouchInstallActivity.class));
+        } else { 
+        	bindService(new Intent("org.couchdb.android.COUCHDB_SERVICE"), mConnection, Context.BIND_AUTO_CREATE);
+        }
 	}
 }
--- src/org/couchdb/android/CouchService.java
+++ src/org/couchdb/android/CouchService.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -14,16 +15,45 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 import com.google.ase.Exec;
 
 public class CouchService extends Service {
+	
 	final String TAG = "CouchDB";
 	final int NO_NOTIFY = 1;
 	private NotificationManager mNM;
+	
+	ArrayList<Messenger> mClients = new ArrayList<Messenger>();
+	
+	static final int MSG_REGISTER_CLIENT = 1;
+	static final int MSG_COUCH_STARTED = 2;
+
 	CouchProcess couch;
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    mClients.add(msg.replyTo);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+    
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
 	
 	public class CouchProcess {
 
@@ -56,6 +86,18 @@ public class CouchService extends Service {
 		        	Log.v(TAG, line);
 		        	if(line.contains("has started on"))
 		        	{
+		        		for (int i=mClients.size()-1; i>=0; i--) {
+	                        try {
+	        		        	Log.v(TAG, "Sending Started Message");
+	                            mClients.get(i).send(Message.obtain(null, MSG_COUCH_STARTED, 0, 0));
+	                        } catch (RemoteException e) {
+	                            // The client is dead.  Remove it from the list;
+	                            // we are going through the list from back to front
+	                            // so this is safe to do inside the loop.
+	                            mClients.remove(i);
+	                        }
+	                    }
+
 		        		Log.v(TAG, "Couch has started.");
 		        		int icon = R.drawable.icon;
 		                CharSequence tickerText = "CouchDB Running";
@@ -80,7 +122,23 @@ public class CouchService extends Service {
 		  }
 		}
 
-	
+	@Override
+    public void onCreate() {
+    	mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        couch = new CouchProcess();
+        int icon = R.drawable.icon;
+        CharSequence tickerText = "CouchDB Starting";
+        long when = System.currentTimeMillis();
+        if(true) {
+        	Notification notification = new Notification(icon, tickerText, when);
+        	Intent notificationIntent = new Intent(this, CouchDB.class);
+        	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        	notification.setLatestEventInfo(getApplicationContext(), "CouchDB Starting", "Please Wait...", contentIntent);
+        	mNM.notify(1, notification);
+        }
+        couch.start("/system/bin/sh", "/sdcard/couch/bin/couchdb", "", true);
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
     	if(couch != null) return 0;
@@ -113,11 +171,16 @@ public class CouchService extends Service {
     	mNM.cancelAll();
     }
 	
-	@Override
-	public IBinder onBind(Intent intent) {
-		this.onStartCommand(null, NO_NOTIFY, 0);
-		return mBinder;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+	//@Override
+	//public IBinder onBind(Intent intent) {
+	//	this.onStartCommand(null, NO_NOTIFY, 0);
+	//	return mBinder;
+	//}
 
 	private final ICouchService.Stub mBinder = new ICouchService.Stub() 
 	{
